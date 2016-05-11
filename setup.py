bf3b264cc327a40fac301dc47d58ffaac8fc00f8
Juan Manuel Berros <juanmaberros@gmail.com>
2016-05-11 20:20:12 -0300

    make biocodices a module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='biocodices',
+      version='0.1',
+      description='Nac & Pop Next-Generation Sequencing',
+      url='http://github.com/biocodices/biocodices',
+      author='Juan Manuel Berros',
+      license='MIT',
+      packages=['biocodices'],
+      zip_safe=False)
