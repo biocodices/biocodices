3ea9f64837d69f1ce78b9e26b0376b466c2fcdc5
Juan Manuel Berros <juanmaberros@gmail.com>
2016-05-12 01:25:03 -0300

    Sample and Sequencing classes basic behavior

--- biocodices/helpers/config.py
+++ biocodices/helpers/config.py
@@ -1,10 +1,10 @@
 import yaml
 
-from os.path import join
+from os.path import join, expanduser
 
 
 class Config:
-    BASE_DIR = 'settings'
+    BASE_DIR = expanduser('~/biocodices/settings')
 
     def __new__(self, config_label):
         return yaml.load(open(join(self.BASE_DIR, config_label + '.yml')))
