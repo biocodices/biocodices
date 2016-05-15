1fed7e45aa7d7d8c66e9024d0271434590008297
Juan Manuel Berros <juanmaberros@gmail.com>
2016-05-15 19:08:29 -0300

    VariantFetcher with Ensemble and Myvariant parsers

--- biocodices/models/declaration.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-
-Base = declarative_base()
-
-from .variation import Variation
-
---
+++ biocodices/web_fetchers/myvariant_parser.py
@@ -0,0 +1,95 @@
+class MyvariantParser:
+    @classmethod
+    def variant(cls, myvariant_series):
+        var_dict = {}
+        functions = [cls._parse_dbsnp_fields,
+                     cls._parse_dbnsfp_fields,
+                     cls._parse_grasp_fields,
+                     cls._parse_gwassnps_fields]
+
+        for function in functions:
+            var_dict.update(function(myvariant_series))
+
+        return var_dict
+
+    @classmethod
+    def publications(cls, myvariant_series):
+        publications = []
+        publications += cls._parse_grasp_publications_field(myvariant_series)
+        return publications
+
+    @staticmethod
+    def _parse_grasp_publications_field(s):
+        if len(s.filter(regex='grasp').index) == 0:
+            return []
+
+        publications = []
+        for pub in s['grasp.publication']:
+            publications.append({
+                'pubmed_id': pub['pmid'],
+                'title': pub['title'],
+                'phenotype': ''.join(pub['phenotype']),
+                # ^ applies to str and lists
+                'date': pub['date_pub'],
+                'note': '',
+                'review': '',
+            })
+
+        return publications
+
+    @staticmethod
+    def _parse_dbsnp_fields(s):
+        if len(s.filter(regex='dbsnp').index) == 0:
+            return {}
+
+        return {
+            'name': s.name,
+            'source': 'dbsnp',
+            'hg19_start': s['dbsnp.hg19.start'],
+            'hg19_end': s['dbsnp.hg19.end'],
+            'chromosome': s['dbsnp.chrom'],
+            'alleles': [d['allele'] for d in s['dbsnp.alleles']],
+            'ref': s['dbsnp.ref'],
+            'alt': s['dbsnp.alt'],
+        }
+
+    @staticmethod
+    def _parse_grasp_fields(s):
+        if len(s.filter(regex='grasp').index) == 0:
+            return {}
+
+        return {
+            'polyphen2': s['grasp.polyphen2'],
+            'sift': s['grasp.sift'],
+            'mapped_gene_grasp': s['grasp.in_gene'],
+        }
+
+    @staticmethod
+    def _parse_gwassnps_fields(s):
+        if len(s.filter(regex='gwassnps').index) == 0:
+            return {}
+
+        return {
+            'gwassnps_pubmed': s['gwassnps.pubmed'],
+            'gwassnps_pvalue': s['gwassnps.pvalue'],
+            'risk_allele': s['gwassnps.risk_allele'],
+            'gwassnps_title': s['gwassnps.title'],
+            'gwassnps_trait': s['gwassnps.trait'],
+        }
+
+    @staticmethod
+    def _parse_dbnsfp_fields(s):
+        if len(s.filter(regex='dbnsfp').index) == 0:
+            return {}
+
+        return {
+            'ancestral_allele': s['dbnsfp.ancestral_allele'].upper(),
+            'strand': s['dbnsfp.cds_strand'],
+            'polyphen2_hdiv_prediction': s['dbnsfp.polyphen2.hdiv.pred'],
+            'polyphen2_hdiv_score': s['dbnsfp.polyphen2.hdiv.score'],
+            'polyphen2_hdiv_rankscore': s['dbnsfp.polyphen2.hdiv.rankscore'],
+            'polyphen2_hvar_prediction': s['dbnsfp.polyphen2.hvar.pred'],
+            'polyphen2_hvar_score': s['dbnsfp.polyphen2.hvar.score'],
+            'polyphen2_hvar_rankscore': s['dbnsfp.polyphen2.hvar.rankscore'],
+            'sift_prediction': s['dbnsfp.sift.pred'],
+        }
