304bfd02085d3352dffc5c1fb0fbd09a7b3df81d
Juan Manuel Berros <juanmaberros@gmail.com>
2016-05-31 14:55:24 -0300

    improved reading the plink association tests results files

--- biocodices/analyzers/association_tester.py
+++ biocodices/analyzers/association_tester.py
@@ -11,6 +11,8 @@ class AssociationTester:
         self.plotter = AssociationTestPlotter(self)
         self.plink = Plink(self.dataset.label_path)
         self.tests = Plink.config('association_tests')
+        self.result_files = {}
+        self.results = {}
 
     def __repr__(self):
         return '<{} for dataset "{}">'.format(self.__class__.__name__,
@@ -27,23 +29,47 @@ class AssociationTester:
 
     def read_plink_test(self, test_name, path):
         interesting_col = self.tests[test_name]['interesting_column']
+        test_label = basename(path)
 
-        if test_name in ['five_models_fisher_for_case_control']:
-            return self.read_model_file(path, interesting_col)
+        if test_name in ['five_models_fisher_for_case_control',
+                         'five_models_chisq_for_case_control']:
+            self.result_files[test_label] = path
+            all_models, p_values_df = self.read_model_file(path,
+                                                           interesting_col)
+            self.results[test_label] = all_models
+            return p_values_df
 
         if test_name in ['linear_regression_for_quanti_trait']:
-            return self.read_qassoc_means_file(path, interesting_col)
+            p_values_df = self.read_qassoc_means_file(path, interesting_col)
+            self.result_files[test_label] = p_values_df
+            self.results[test_label] = p_values_df
+            return p_values_df
 
     @staticmethod
-    def read_model_file(path, interesting_col):
+    def _calc_cc_ratio(df):
+        # I take the first row to check affected vs affected since every row
+        # will show the same sum of cases and controls. The exception would be
+        # the ALLELIC test, where the sum is 2N, but the ratio will be the
+        # same one anyway.
+        first_row = df.iloc[0]
+        n_cases = sum([int(n) for n in first_row['AFF'].split('/')])
+        n_controls = sum([int(n) for n in first_row['UNAFF'].split('/')])
+        return n_cases, n_controls, round(n_cases / n_controls, 2)
+
+    @classmethod
+    def read_model_file(cls, path, interesting_col):
         df = pd.read_table(path, sep='\s+')
         df.set_index(['CHR', 'SNP'], inplace=True)
+        n_cases, n_controls, cc_ratio = cls._calc_cc_ratio(df)
+        df.loc[:, 'n_cases'] = n_cases
+        df.loc[:, 'n_controls'] = n_controls
+        df.loc[:, 'cc_ratio'] = cc_ratio
         ret = pd.DataFrame({})
         for model, df_model in df.groupby('TEST'):
             series = df_model[interesting_col]
             series.name = '{}_{}'.format(model, interesting_col)
             ret = ret.append(series)
-        return ret.transpose()
+        return df, ret.transpose()
 
     @staticmethod
     def read_qassoc_means_file(path, interesting_col):
