9a642b1ed32bbf36e162baa79ed0c79cd35a7462
Juan Manuel Berros <juanmaberros@gmail.com>
2016-06-03 15:47:24 -0300

    Better parsing of MyVariant annotations

--- biocodices/web_fetchers/variant_annotator.py
+++ biocodices/web_fetchers/variant_annotator.py
@@ -1,40 +1,44 @@
 import requests
 import sys
+from collections import namedtuple
 from myvariant import MyVariantInfo
 
 from biocodices.web_fetchers import MyvariantParser, EnsembleParser
 
 
 class VariantAnnotator:
-    def run(self):
-        self.myvariant_series = self._query_myvariant()
-        self.ensemble_dict = self._query_ensemble()
-        self._parse()
+    def run(self, rs):
+        """Query MyVariant.info and Ensemble for info about an rs ID."""
+        return self._parse_fetched_data(
+            myvariant_df=self._query_myvariant(rs),
+            ensemble_dict=self._query_ensemble(rs),
+        )
 
-    def _parse(self):
-        myvariant_var = MyvariantParser.variant(self.myvariant_series)
-        ensemble_var = EnsembleParser.variant(self.ensemble_dict)
-        self.variant = {**myvariant_var, **ensemble_var}
+    @staticmethod
+    def _parse_fetched_data(myvariant_df, ensemble_dict):
+        Info = namedtuple('VariantAnnotation',
+                          ['myvariant', 'ensemble', 'publications'])
 
-        myvariant_pubs = MyvariantParser.publications(self.myvariant_series)
-        ensemble_pubs = EnsembleParser.publications(self.ensemble_dict)
-        self.publications = myvariant_pubs + ensemble_pubs
+        variant_df = MyvariantParser.parse_annotations(myvariant_df)
+        # EnsembleParser.variant(self.ensemble_dict)
 
-    def _query_myvariant(self):
-        fields = ['dbsnp', 'dbnsfp', 'grasp', 'gwassnps']
+        myvariant_pubs = MyvariantParser.publications(myvariant_df)
+        ensemble_pubs = EnsembleParser.publications(ensemble_dict)
+        publications = myvariant_pubs + ensemble_pubs
+
+        return Info(variant_df, ensemble_dict, publications)
+
+    def _query_myvariant(self, rs):
+        # fields = ['dbsnp', 'dbnsfp', 'grasp', 'gwassnps']
+        fields = ['all']
         mv = MyVariantInfo()
-        df = mv.query(self.rs_id, fields=fields, as_dataframe=True)
-        df.set_index('dbsnp.rsid', inplace=True)
+        df = mv.query(rs, fields=fields, as_dataframe=True)
+        df.set_index(['dbsnp.rsid', '_id'], inplace=True)
+        return df
 
-        if len(df.index) > 1:
-            msg = 'MyVariant.info returned many entries for {}: {}'
-            raise Exception(msg.format(self.rs_id, df.index))
-
-        return df.iloc[0]
-
-    def _query_ensemble(self):
+    def _query_ensemble(self, rs):
         server = "http://rest.ensembl.org"
-        ext = "/variation/human/{}?phenotypes=1".format(self.rs_id)
+        ext = "/variation/human/{}?phenotypes=1".format(rs)
 
         headers = { "Content-Type" : "application/json"}
         r = requests.get(server + ext, headers=headers)
