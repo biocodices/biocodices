b7f7552bcad7f4707362142e49bac50e38baa3ae
Juan Manuel Berros <juanmaberros@gmail.com>
2016-06-05 00:30:43 -0300

    BcfTools bgzip, tabix, limit VCF

--- biocodices/programs/bcf_tools.py
+++ biocodices/programs/bcf_tools.py
@@ -1,6 +1,6 @@
 from os.path import dirname, join
 
-from biocodices.helpers import Config
+from biocodices.helpers import Config, Resource
 from biocodices.programs import AbstractGenomicsProgram, ProgramCaller
 
 
@@ -9,11 +9,11 @@ class BcfTools(AbstractGenomicsProgram):
         self.label = 'bcftools'
         super(self.__class__, self).__init__(self.label)
 
-    def run(self, module_name, infile, outfile, params_str=None):
+    def run(self, module_name, infile, outfile, params_str=None,
+            log_label=None):
         command = '{} {} {} {}'.format(self.executable, module_name,
                                        (params_str or ''), infile)
-
-        log_label = '{}_{}'.format(self.label, module_name)
+        log_label = log_label or '{}_{}'.format(self.label, module_name)
         log_filepath = join(dirname(outfile), log_label)
         ProgramCaller(command).run(stdout_sink=outfile,
                                    log_filepath=log_filepath)
@@ -26,14 +26,20 @@ class BcfTools(AbstractGenomicsProgram):
         - outfile: an absolute path for the new vcf file.
         """
         params_str = '--samples ' + ','.join(sample_ids)
-        self.run('view', vcf_path, outfile, params_str=params_str)
+        self.run('view', vcf_path, outfile, params_str=params_str,
+                 log_label='bcftools_view_samples')
+        return outfile
 
     def limit_samples(self, gz_vcf_path):
         """
-        Subset a VCF keeping the regions contained in the BED defined as
-        'panel_amplicons' in the resources.yml file.
+        Subset a VCF.gz keeping the regions contained in the BED defined as
+        'panel_amplicons' in the resources.yml file. Output a VCF.
         """
-        pass
+        outfile = gz_vcf_path.replace('.vcf.gz', '.lim.vcf')
+        params_str = '--regions-file {}'.format(Resource('panel_amplicons'))
+        self.run('view', gz_vcf_path, outfile, params_str=params_str,
+                 log_label='bcftools_view_regions')
+        return outfile
 
     @staticmethod
     def tabix(gz_vcf_path):
@@ -42,7 +48,7 @@ class BcfTools(AbstractGenomicsProgram):
         command = '{} {}'.format(executable, gz_vcf_path)
         log_filepath = join(dirname(gz_vcf_path), 'tabix')
         ProgramCaller(command).run(log_filepath=log_filepath)
-        return gz_vcf_path + '.tbi'
+        return gz_vcf_path
 
     @staticmethod
     def bgzip(vcf_path):
