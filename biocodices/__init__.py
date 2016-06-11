ca42afcad757c1f1978a14e3f70803285affd695
Juan Manuel Berros <juanmaberros@gmail.com>
2016-06-11 10:06:22 -0300

    version

--- biocodices/__init__.py
+++ biocodices/__init__.py
@@ -6,6 +6,6 @@ matplotlib.use('Agg')
 # remote server with no X. This line has to be executed before importing
 # pyplot, so we have to run it before any biocodices code is imported.
 
-__version__ = '0.3'
+__version__ = '0.3.1'
 __program_name__ = 'biocodices'
 software_name = '{} {}'.format(__program_name__, __version__)
