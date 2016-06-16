c59ab344a3c570908a1e9886f7b9f0e3035c89ab
Juan Manuel Berros <juanmaberros@gmail.com>
2016-06-15 22:58:07 -0300

    fixed matplotlib use() to run when no DISPLAY

--- biocodices/__init__.py
+++ biocodices/__init__.py
@@ -1,10 +1,13 @@
-import matplotlib
+import os
 
-# Force matplotlib to not use any Xwindows backend.
-matplotlib.use('Agg')
-# This prevents matplotlib raising an exception when running biocodices on a
-# remote server with no X. This line has to be executed before importing
-# pyplot, so we have to run it before any biocodices code is imported.
+
+if 'DISPLAY' not in os.environ:
+    import matplotlib
+    # Force matplotlib to not use any Xwindows backend.
+    matplotlib.use('Agg')
+    # This prevents matplotlib from raising an exception when running
+    # on a remote server with no X. This line has to be executed before
+    # importing pyplot.
 
 __version__ = '0.3.1'
 __program_name__ = 'biocodices'
