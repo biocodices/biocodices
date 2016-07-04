2d4c905c7cab7f404ec68d8cab9059466b21017f
Juan Manuel Berros <juanmaberros@gmail.com>
2016-07-04 17:54:12 -0300

    add db method to merge parkinson tables of variants and proteins

--- biocodices/helpers/db.py
+++ biocodices/helpers/db.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -30,3 +31,24 @@ class DB:
     def tables(self):
         """Show table names from the current database."""
         return [t[0] for t in self.query('show tables;', as_list=True)]
+
+    def protein_changes_by_rs(self):
+        """Biocodices-parkinsonDB specific merging of tables."""
+        df = self.table('mutations_report')
+        df = df.loc[:, ['VariationName', 'GeneID','TypeVariation', 'GenomicChange', 'ProteinChange']]
+
+        def parse_protein_change(change, ret):
+            match = re.search(r'p.(\D{3})(\d+)(\D{3}|=)', change)
+            if not match: return ''
+            data = {'normal_aminoacid': match.group(1),
+                    'protein_position': match.group(2),
+                    'new_aminoacid': match.group(3)}
+            return data[ret]
+
+        df['Normal Aminoacid'] = df['ProteinChange'].map(lambda code: parse_protein_change(code, 'normal_aminoacid'))
+        df['New Aminoacid'] = df['ProteinChange'].map(lambda code: parse_protein_change(code, 'new_aminoacid'))
+        df['Protein Position'] = df['ProteinChange'].map(lambda code: parse_protein_change(code, 'protein_position'))
+
+        protein_changes_by_rs = df.sort_values(by='GeneID').reset_index(drop=True)
+
+        return protein_changes_by_rs
