cd02e576a793f9e93f275a228116f530080d009b
Juan Manuel Berros <juanmaberros@gmail.com>
2016-10-28 10:58:04 -0300

    add docstring to make_chromosome_series_categorical

--- biocodices/helpers/general.py
+++ biocodices/helpers/general.py
@@ -8,6 +8,10 @@ import numpy as np
 
 
 def make_chromosome_series_categorical(series):
+    """
+    Receives a pandas Series with chromosomes and returns a new categorical
+    Series with the same values.
+    """
     chromosomes = [str(chrom) for chrom in range(1, 23)] + ['X', 'Y']
     new_series = series.replace(23, 'X').replace(24, 'Y').astype(str)
     new_series = new_series.astype('category', categories=chromosomes,
