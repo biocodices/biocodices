86cea0f33b8359c494ee53a7ed70ce49ac2bdada
Juan Manuel Berros <juanmaberros@gmail.com>
2016-11-14 12:46:13 -0300

    commented out pipeline related imports

--- biocodices/components/__init__.py
+++ biocodices/components/__init__.py
@@ -1,6 +1,6 @@
 from .project import Project
-from .sample import Sample
-from .cohort import Cohort
-from .sample_group import SampleGroup
-from .dataset import Dataset
-from .phenotype_dataset import PhenotypeDataset
+#  from .sample import Sample
+#  from .cohort import Cohort
+#  from .sample_group import SampleGroup
+#  from .dataset import Dataset
+#  from .phenotype_dataset import PhenotypeDataset
